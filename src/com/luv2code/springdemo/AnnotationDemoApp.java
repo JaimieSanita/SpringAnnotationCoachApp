a6e826a220dd510b047d9a701a96cc999f02ed7a
Jaimie Sanita <jsanita11@gmail.com>
2021-05-31 15:53:14 -0400

    first commit

---
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,20 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		//read spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");	
+		//get the bean from spring container //bean id	//interface
+		Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+		
+		//call methods on bean
+		System.out.println(theCoach.getDailyWorkout());
+		//close the context
+		context.close();
+
+	}
+
+}
