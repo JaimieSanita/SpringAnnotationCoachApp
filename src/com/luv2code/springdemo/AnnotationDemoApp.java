c1feb0dfc77afd3f3c206116c4d72709873b42c7
Jaimie Sanita <jsanita11@gmail.com>
2021-05-31 16:01:17 -0400

    using default bean id

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -8,7 +8,7 @@ public class AnnotationDemoApp {
 		//read spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");	
 		//get the bean from spring container //bean id	//interface
-		Coach theCoach = context.getBean("thatSillyCoach", Coach.class);
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		
 		//call methods on bean
 		System.out.println(theCoach.getDailyWorkout());
