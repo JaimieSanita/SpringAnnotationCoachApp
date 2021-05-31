5163bec6da29eb87f9be0958cb8c495c1e94c087
Jaimie Sanita <jsanita11@gmail.com>
2021-05-31 16:09:33 -0400

    inversion of control with swimming coach

---
+++ src/com/luv2code/springdemo/SwimmingCoach.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SwimmingCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Practice the backstroke for 20 minutes";
+	}
+
+}
